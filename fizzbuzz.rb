d2ff2766c29d792a7740f2f480551300e59e68e9
Natasha Chernis <natashachernis@gmail.com>
2019-02-09 23:00:00 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,10 @@
 def fizzbuzz(number)
   if number % 3 == 0
     "Fizz"
+    if number % 5 == 0
+      "FizzBuzz"
   elsif number % 5 == 0
     "Buzz"
-  elsif number % 15 == 0
-    "FizzBuzz"
   else
     nil
   end
